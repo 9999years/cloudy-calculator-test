9e6f63f6a2db8d8bdc4304ba180b7132c1d62717
Rebecca Turner <637275@gmail.com>
2019-07-13 12:58:39 -0700

    who tf knows

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,14 @@ const path = require('path')
 const webpack = require('webpack')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const InertEntryPlugin = require('inert-entry-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 const IS_PRODUCTION = process.env.NODE_ENV === 'production'
 
 const DIST_DIR = path.resolve(__dirname, 'dist')
 const SRC_DIR = path.resolve(__dirname, 'src')
+const SCRIPT_DIR = path.join(SRC_DIR, 'scripts')
+const STYLE_DIR = path.join(SRC_DIR, 'styles')
 const ASSET_EXTENSIONS = ['jpg', 'jpeg', 'png', 'gif', 'eot', 'otf', 'svg', 'ttf', 'woff', 'woff2']
 const MANIFEST_FILE = 'manifest.json'
 
@@ -45,8 +48,8 @@ module.exports = {
           {
             loader: 'file-loader',
             options: {
-              name: '[hash].css',
-            },
+              name: '[name].css',
+            }h
           },
           'extract-loader',
           'css-loader',
@@ -54,44 +57,13 @@ module.exports = {
         ]
       },
       {
-        test: /\/index\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: [{
-          loader: 'spawn-loader',
-          options: {
-            name: '[hash].js'
-          }
-        }]
-      },
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
         use: [
           {
-            loader: 'babel-loader',
-            options: {
-              presets: [
-                ['@babel/preset-env', { modules: false }]
-              ]
-            }
-          },
-          // Ensure babel-polyfill is imported
-          // Normally, this would just be an entry point, but relying on
-          // spawn-loader is preventing us from doing that.
-          {
-            loader: 'imports-loader',
-            query: '__babelPolyfill=babel-polyfill'
+            loader: 'ts-loader',
           }
         ]
-      },
-      {
-        test: new RegExp('\\.(' + ASSET_EXTENSIONS.join('|') + ')$'),
-        use: {
-          loader: 'file-loader',
-          options: {
-            outputPath: 'assets/'
-          }
-        }
       },
       // Workaround for https://github.com/webpack/webpack/issues/5828
       {
@@ -106,7 +78,9 @@ module.exports = {
     new webpack.ProvidePlugin({
       browser: 'webextension-polyfill'
     }),
-    //new ForkTsCheckerWebpackPlugin(),
+    new CopyPlugin([
+      { from: path.join(SRC_DIR, 'icon'), to: path.join(DIST_DIR, 'icon') },
+    ]),
   ],
   devtool: IS_PRODUCTION ? '' : 'inline-source-map'
 }
